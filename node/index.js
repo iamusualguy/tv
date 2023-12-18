f5ba9d55925e1cc683c3a023176d70214f7dae73
Anton <iamusualguy@gmail.com>
2023-12-18 17:55:53 +0100

    small upd from the plane

--- node/index.js
+++ node/index.js
@@ -5,29 +5,35 @@ const { spawn } = require('child_process');
 
 const app = express();
 
-// app.register('.html', require('jade'))
-
 const videoFolder = '../video';
+const resolution = '640:480';
+const tvName = "usual tv";
 let videoQueue = [];
 let currentIndex = 0;
 let currentProcess = null;
 
+
+function refillQueue() {
+  console.log("refill queue");
+  videoQueue = fs.readdirSync(videoFolder).filter(file => file.endsWith('.mp4'));
+}
+
 function startNextVideo() {
-  if (videoQueue.length === 0) {
+  if (videoQueue.length === 0 || currentIndex == 0) {
     refillQueue();
   }
   if (videoQueue.length > 0) {
     const videoFile = videoQueue[currentIndex];
-    console.log(videoFile);
     const command = [
-      // 'ffmpeg',
       '-re',
       '-i',
       path.join(videoFolder, videoFile),
       '-c:v',
       'libx264',
+      "-preset",
+      "ultrafast",
       '-vf',
-      "[in]scale=320:240:force_original_aspect_ratio=decrease,pad=320:240:(ow-iw)/2:(oh-ih)/2,drawtext=fontsize=25:fontcolor=white:text='пися палыч тв':x=25:y=25,drawtext=fontsize=18:fontcolor=white:text='%{localtime\\:%T}':x=25:y=55[out]",
+      `[in]scale=${resolution}:force_original_aspect_ratio=decrease,pad=${resolution}:(ow-iw)/2:(oh-ih)/2,drawtext=fontsize=25:fontcolor=white:text='${tvName}':x=25:y=25,drawtext=fontsize=18:fontcolor=white:text='%{localtime\\:%T}':x=25:y=55[out]`,
       '-hls_time',
       '1',
       '-hls_list_size',
@@ -36,40 +42,24 @@ function startNextVideo() {
       'hls',
       '-segment_wrap',
       '6',
-      // '-hls_flags',
-      // 'append_list+omit_endlist',
       '-hls_flags',
       'delete_segments+append_list+omit_endlist',
-      // '-hls_delete_threshold',
-      // '5',
       'static/stream.m3u8',
     ];
     currentProcess = spawn('ffmpeg', command);
+    console.log(currentIndex, videoFile);
     currentProcess.on('close', (code) => {
-
     currentIndex = (currentIndex + 1) % videoQueue.length;
+      removeOldTSFiles("./static/");
       startNextVideo();
     });
-    // currentProcess.on('exit', startNextVideo);
   }
 }
 
-function refillQueue() {
-  videoQueue = fs.readdirSync(videoFolder).filter(file => file.endsWith('.mp4'));
-}
 
-// app.get('/', (req, res) => {
-//   const data = {
-//     videoQueue: videoQueue,
-//     current_index: currentIndex,
-//   };
-//   res.render('index', data);
-// });
 
 app.get('/', (req, res) => {
-  // res.writeHead(200, {'Content-Type': 'text/html'})
   res.sendFile(__dirname + '/index.html');
-  // res.end()
 })
 
 function start() {
@@ -81,11 +71,6 @@ function start() {
   startNextVideo();
   
 }
-
-app.get('/start', (req, res) => {
-  start();
-  res.send('Streaming started');
-});
 
 app.use('/static', express.static('static'));
 
@@ -101,3 +86,46 @@ app.listen(3000, () => {
   console.log('Server running on port 3000');
   start();
 });
+
+
+function removeOldTSFiles(directoryPath) {
+  const playlistFile = path.join(directoryPath, 'stream.m3u8');
+  
+  // Read the content of the playlist file
+  fs.readFile(playlistFile, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading playlist file:', err);
+      return;
+    }
+
+    // Extract TS file names from the playlist
+    const tsFiles = data.match(/stream\d+\.ts/g);
+
+    if (!tsFiles || tsFiles.length === 0) {
+      console.log('No TS files found in the playlist.');
+      return;
+    }
+
+    // Get all TS files in the directory
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        console.error('Error reading directory:', err);
+        return;
+      }
+
+      // Filter out TS files that are not listed in the playlist
+      const obsoleteTSFiles = files.filter(file => file.startsWith('stream') && file.endsWith('.ts') && !tsFiles.includes(file));
+
+      // Remove obsolete TS files
+      obsoleteTSFiles.forEach(file => {
+        fs.unlink(path.join(directoryPath, file), err => {
+          if (err) {
+            console.error(`Error removing ${file}:`, err);
+          } else {
+            // console.log(`${file} has been removed.`);
+          }
+        });
+      });
+    });
+  });
+}
